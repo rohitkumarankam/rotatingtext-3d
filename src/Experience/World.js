4af61145eea80fb94dff4bff7013c3fb63efec86
Rohit <rohitkumarankam@gmail.com>
2021-10-11 20:28:13 +0530

    first commit

---
+++ src/Experience/World.js
@@ -0,0 +1,52 @@
+import * as THREE from 'three'
+import Experience from './Experience.js'
+
+export default class World
+{
+    constructor(_options)
+    {
+        this.experience = new Experience()
+        this.config = this.experience.config
+        this.scene = this.experience.scene
+        this.resources = this.experience.resources
+        
+        this.resources.on('groupEnd', (_group) =>
+        {
+            if(_group.name === 'base')
+            {
+                this.setDummy()
+            }
+        })
+    }
+
+    setDummy()
+    {   
+        const texture = this.resources.items.baked1
+        texture.flipY = false
+        const material = new THREE.MeshBasicMaterial({map:texture})
+        const cube = new THREE.Mesh(
+            new THREE.BoxGeometry(1, 1, 1),
+            new THREE.MeshBasicMaterial({ map: this.resources.items.lennaTexture })
+        )
+        this.scene.add(this.resources.items.path.scene)
+        this.resources.items.path.scene.traverse((child)=>{
+            child.material = material
+        })
+
+        cube.position.setY(0.5)
+
+        this.scene.add(cube)        
+    }
+
+    resize()
+    {
+    }
+
+    update()
+    {
+    }
+
+    destroy()
+    {
+    }
+}
---
+++ src/Experience/assets.js
@@ -0,0 +1,12 @@
+export default [
+    {
+        name: 'base',
+        data: {},
+        items:
+        [
+            { name: 'lennaTexture', source: '/assets/lenna.png', type: 'texture' },
+            { name: 'path', source: '/assets/path.glb'},
+            { name: 'baked1', source: '/assets/baked1.jpg', type: 'texture'},
+        ]
+    }
+]
